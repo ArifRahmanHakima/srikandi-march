4c9e94d662228a12b5cf5b6564d128f4db23c422
Wahyuu194 <2200018194@webmail.uad.ac.id>
2025-05-01 20:16:12 +0700

    edit bg

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
-    <body class="bg-slate-200 dark:bg-slate-700">
+    <body class="bg-slate-200 white:bg-slate-700">
         @livewire('partials.navbar')
         <main>
             {{ $slot }}
